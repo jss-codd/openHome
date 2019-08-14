0b5570a13c0e1f9296ccf8c426fa0dee86ab7d47
aniket <pandeyaniket546@gmail.com>
2019-08-14 18:13:11 +0530

    do

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-/*server.js*/
-const http = require('http');
-const hostname = 'ec2-54-191-104-59.us-west-2.compute.amazonaws.com';
-const port = 8080;
-const server = http.createServer(function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(port, hostname, function() {
-  console.log('Server running at http://'+ hostname + ':' + port + '/');
+const express = require("express");
+
+
+
+
+process.on('unhandledRejection', (reason, p) => {
+	console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
 });
 
+var app = express();
+
+
+
+
+// app.use(require("./app/middleware/authInterceptor").authorizeApi);
+app.get('/',(req,res)=>{
+    res.send("Weclome to AWS EC2")
+});
+app.listen(parseInt(process.env.PORT, 10) || 3000, () => {
+	console.log("App running");
+});
